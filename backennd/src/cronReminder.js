9b7320ad2693e663871bbfcda2827f6c055e1042
warodom-real <koboro0@gmail.com>
2026-03-16 19:30:34 +0700

    almost last setup for email

--- backennd/src/cronReminder.js
+++ backennd/src/cronReminder.js
@@ -1,9 +1,8 @@
 const cron = require("node-cron");
 const db = require("./db");
-const { sendReminder } = require('./emailService')
+const { sendReminder } = require("./emailService");
 
-cron.schedule("*/30 * * * * *", () => {
-
+cron.schedule("*/10 * * * * *", () => {
     console.log("checking books...");
 
     const sql = `
@@ -16,27 +15,26 @@ cron.schedule("*/30 * * * * *", () => {
         JOIN users u ON bs.user_id = u.id
         JOIN books b ON bs.book_id = b.id
         WHERE bs.status='borrowed'
-        AND TIMESTAMPDIFF(HOUR, NOW(), bs.end_date) <= 24
-        `;
-    db.query(sql, async (err, results) => {
+        AND bs.reminded = 0
+        AND TIMESTAMPDIFF(HOUR, DATE_ADD(NOW(), INTERVAL 7 HOUR), bs.end_date) <= 24
+        AND TIMESTAMPDIFF(HOUR, DATE_ADD(NOW(), INTERVAL 7 HOUR), bs.end_date) >= 0
+    `;
 
+    db.query(sql, async (err, results) => {
         if (err) {
             console.log(err);
             return;
         }
-        console.log(results)
-        for (let row of results) {
 
+        console.log(results);
+
+        for (let row of results) {
             console.log("Sending email to:", row.email);
 
-            await sendReminder(
-                row.email,
-                row.book_name,
-                row.end_date
-            );
+            await sendReminder(row.email, row.book_name, row.end_date);
 
+            // มาร์คว่าส่งแล้ว ไม่ส่งซ้ำ
+            db.query("UPDATE booked_system SET reminded = 1 WHERE id = ?", [row.id]);
         }
-
     });
-
 });
